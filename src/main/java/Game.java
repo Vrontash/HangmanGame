8ec48a9d45f1e0da9fa31c5e67f46671eb231914
Vlad <vladmelnikov078@gmail.com>
2024-08-19 23:10:03 +0300

    Implemented a random word selection from the file

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,40 @@
 package src.main.java;
+import java.io.*;
+
+import java.util.ArrayList;
+
 
 public class Game {
+    private final String WORDS_FILENAME = "src/main/resources/russian_nouns.txt";
     private final HangmanCLI gameCLI;
 
     Game(HangmanCLI gameCLI){
         this.gameCLI = gameCLI;
     }
-    public void start(){}
+    public ArrayList <String> readWordsFile(String fileName){
+        ArrayList<String> listOfWords = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null)
+                listOfWords.add(line);
+        } catch (IOException e) {
+            System.out.println("ERROR: File with words cannot be found or read");
+            System.exit(-1);
+        }
+        return listOfWords;
+    }
+    public String chooseWord(ArrayList<String> listOfWords){
+        int wordIndex = (int) (Math.random() * listOfWords.size());
+
+        return listOfWords.get(wordIndex);
+    }
+    public void start(){
+        ArrayList<String> listOfWords = readWordsFile(WORDS_FILENAME);
+        System.out.println(listOfWords);
+        gameCLI.printHangman(0);
+        String hiddenWord = chooseWord(listOfWords);
+        System.out.println(hiddenWord);
+    }
 }
